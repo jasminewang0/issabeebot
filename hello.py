53c29638ae9c5497d39994d4e6ebdac0dd40ec2f
Jasmine <jfwang2@ncsu.edu>
2017-11-04 16:36:24 -0400

    it works, queries up to 100 tweets and tweets every 10 mins

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@
 import tweepy, time
 #from our keys module (keys.py), import the keys dictionary
 from keys import keys
+from tweepy import *
 
 CONSUMER_KEY = keys['consumer_key']
 CONSUMER_SECRET = keys['consumer_secret']
@@ -12,16 +13,19 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-twt = api.search(q="love bees")
+twt = api.search(q="love bees", count=100)
 
 #list of specific strings we want to check for in Tweets
 t = ['love bees']
+count = 0
 
 for s in twt:
     for i in t:
         if i in s.text:
-            print s.text
             sn = s.user.screen_name
             m = "@%s YA LIKE JAZZ?" % (sn)
-            #s = api.update_status(m, s.id)
-            #time.sleep(10)
+            try:
+                s = api.update_status(m, s.id)
+                time.sleep(600) #wait 10 mins
+            except TweepError:
+                print("Already been tweeted, skip this!")
